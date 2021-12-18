13b2b766499bfd643135e9441d64c648c58d4871
Robert Mikhailovskiy <76881131+morj12@users.noreply.github.com>
2021-12-18 16:11:43 +0100

    Update MainController.java

--- src/main/java/com/morj12/adiiucharts/controller/MainController.java
+++ src/main/java/com/morj12/adiiucharts/controller/MainController.java
@@ -9,26 +9,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-
+/**
+ * MAIN CONTROLLER CLASS
+ *
+ * Catches every hyperlink to local resource, process it
+ * and return a view.
+ * For example, when someone goes to salary/ it gets all the
+ * database entries and saves them into the Model. After that,
+ * it returns a view where information of this Model is used.
+ */
 
 @Controller
 public class MainController {
 
+    /**
+     * Used to connect to the DB.
+     */
     @Autowired
     private CountryRepository countryRepository;
 
+    /**
+     * Home page request.
+     * @return home page view
+     */
     @RequestMapping("/")
     public String indexMapping() {
         return "index.jsp";
     }
 
+    /**
+     * Salary chart request.
+     * @param model used to get countries data
+     * @return salaries page view
+     */
     @RequestMapping("/salary")
     public String salaryMapping(Model model) {
-        List<Country> countryList = countryRepository.findAll();
-        model.addAttribute("countries", countryList);
+        List<Country> countryList = countryRepository.findAll();    // Get all countries
+        model.addAttribute("countries", countryList);   // Save them as "countries"
         return "salary.jsp";
     }
 
+    /**
+     * GDP chart request
+     * @param model used to get countries data
+     * @return GDP page view
+     */
     @RequestMapping("/gdp")
     public String gdpMapping(Model model) {
         List<Country> countryList = countryRepository.findAll();
@@ -36,6 +61,10 @@ public class MainController {
         return "gdp.jsp";
     }
 
+    /**
+     * Covid map request
+     * @return covid page view
+     */
     @RequestMapping("/covid")
     public String covidMapping() {
         return "covid.jsp";
