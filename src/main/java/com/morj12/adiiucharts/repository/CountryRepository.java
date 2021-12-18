360893c8bac2f4c969923cf1d606c9439fcc2458
Robert Mikhailovskiy <76881131+morj12@users.noreply.github.com>
2021-12-18 16:11:59 +0100

    Update CountryRepository.java

--- src/main/java/com/morj12/adiiucharts/repository/CountryRepository.java
+++ src/main/java/com/morj12/adiiucharts/repository/CountryRepository.java
@@ -1,12 +1,19 @@
 package com.morj12.adiiucharts.repository;
 
-
 import com.morj12.adiiucharts.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
+/**
+ * JPA REPOSITORY FOR "COUNTRY"
+ *
+ * This repository is needed because it links the Java
+ * application to a database table. We need to specify
+ * the entity type and the type of primary key.
+ *
+ * Also, we don't need to override methods because this
+ * abstraction already has all methods we need.
+ */
 
 public interface CountryRepository extends JpaRepository<Country, String> {
-    @Override
-    List<Country> findAll();
 }
