885b23af6dc5b459db409c59a85274fa2c0c3de5
Robert Mikhailovskiy <76881131+morj12@users.noreply.github.com>
2021-12-18 16:12:07 +0100

    Update AdiiuChartsApplication.java

--- src/main/java/com/morj12/adiiucharts/AdiiuChartsApplication.java
+++ src/main/java/com/morj12/adiiucharts/AdiiuChartsApplication.java
@@ -3,6 +3,12 @@ package com.morj12.adiiucharts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * MAIN EXECUTABLE
+ *
+ * Starts a Spring Boot application.
+ */
+
 @SpringBootApplication
 public class AdiiuChartsApplication {
 
